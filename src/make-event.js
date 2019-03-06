ea197b36031e5810dd3f893efaf61ba3e752efc9
Andreeva Anastasia <aam.fdev@gmail.com>
2019-03-06 18:30:57 +0300

    m3 t1 improved event template

--- src/make-event.js
+++ src/make-event.js
@@ -1,19 +1,45 @@
-export default () => `
+import {eventData} from './mock';
+
+export const makeEvent = () => {
+  return `
   <article class="trip-point">
-    <i class="trip-icon">🚕</i>
-    <h3 class="trip-point__title">Taxi to Airport</h3>
+    <i class="trip-icon">${eventData.icon}</i>
+    <h3 class="trip-point__title">${eventData.title}</h3>
     <p class="trip-point__schedule">
-      <span class="trip-point__timetable">10:00&nbsp;&mdash; 11:00</span>
-      <span class="trip-point__duration">1h 30m</span>
+      <span class="trip-point__timetable">${eventData.time.from}&nbsp;&mdash; ${eventData.time.to}</span>
+      <span class="trip-point__duration">${eventData.duration}</span>
     </p>
-    <p class="trip-point__price">&euro;&nbsp;20</p>
+    <p class="trip-point__price">&euro;&nbsp;${eventData.price}</p>
     <ul class="trip-point__offers">
       <li>
-        <button class="trip-point__offer">Order UBER +&euro;&nbsp;20</button>
+        <button class="trip-point__offer">${eventData.offers} +&euro;&nbsp;20</button>
       </li>
       <li>
-        <button class="trip-point__offer">Upgrade to business +&euro;&nbsp;20</button>
+        <button class="trip-point__offer">${eventData.offers} +&euro;&nbsp;20</button>
       </li>
     </ul>
   </article>
 `;
+};
+
+export const makeFullEvent = () => {
+  return `
+  <article class="trip-point">
+    <i class="trip-icon">${eventData.icon}</i>
+    <h3 class="trip-point__title">${eventData.title}</h3>
+    <p class="trip-point__schedule">
+      <span class="trip-point__timetable">${eventData.time.from}&nbsp;&mdash; ${eventData.time.to}</span>
+      <span class="trip-point__duration">${eventData.duration}</span>
+    </p>
+    <p class="trip-point__price">&euro;&nbsp;${eventData.price}</p>
+    <ul class="trip-point__offers">
+      <li>
+        <button class="trip-point__offer">Edit</button>
+      </li>
+      <li>
+        <button class="trip-point__offer">Delete</button>
+      </li>
+    </ul>
+  </article>
+`;
+};
