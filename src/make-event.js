4c98955b5a2396a71bca99a7eb99845639508f66
Andreeva Anastasia <aam.fdev@gmail.com>
2019-03-13 13:10:27 +0300

    m3 t1 prepared templates

--- src/make-event.js
+++ src/make-event.js
@@ -1,45 +1,26 @@
-import {eventData} from './mock';
+export default (eventData) => {
 
-export const makeEvent = () => {
-  return `
-  <article class="trip-point">
-    <i class="trip-icon">${eventData.icon}</i>
-    <h3 class="trip-point__title">${eventData.title}</h3>
-    <p class="trip-point__schedule">
-      <span class="trip-point__timetable">${eventData.time.from}&nbsp;&mdash; ${eventData.time.to}</span>
-      <span class="trip-point__duration">${eventData.duration}</span>
-    </p>
-    <p class="trip-point__price">&euro;&nbsp;${eventData.price}</p>
-    <ul class="trip-point__offers">
+  const offersTemplate = [];
+  eventData.offers.forEach(offer => {
+    offersTemplate.push(`
       <li>
-        <button class="trip-point__offer">${eventData.offers} +&euro;&nbsp;20</button>
+        <button class="trip-point__offer">${offer} +&euro;&nbsp;20</button>
       </li>
-      <li>
-        <button class="trip-point__offer">${eventData.offers} +&euro;&nbsp;20</button>
-      </li>
-    </ul>
-  </article>
-`;
-};
+    `);
+  });
 
-export const makeFullEvent = () => {
   return `
-  <article class="trip-point">
-    <i class="trip-icon">${eventData.icon}</i>
-    <h3 class="trip-point__title">${eventData.title}</h3>
-    <p class="trip-point__schedule">
-      <span class="trip-point__timetable">${eventData.time.from}&nbsp;&mdash; ${eventData.time.to}</span>
-      <span class="trip-point__duration">${eventData.duration}</span>
-    </p>
-    <p class="trip-point__price">&euro;&nbsp;${eventData.price}</p>
-    <ul class="trip-point__offers">
-      <li>
-        <button class="trip-point__offer">Edit</button>
-      </li>
-      <li>
-        <button class="trip-point__offer">Delete</button>
-      </li>
-    </ul>
-  </article>
-`;
-};
+    <article class="trip-point">
+      <i class="trip-icon">${eventData.event.icon}</i>
+      <h3 class="trip-point__title">${eventData.event.title} ${eventData.event.location}</h3>
+      <p class="trip-point__schedule">
+        <span class="trip-point__timetable">${eventData.time.from}&nbsp;&mdash; ${eventData.time.to}</span>
+        <span class="trip-point__duration">${eventData.time.duration}</span>
+      </p>
+      <p class="trip-point__price">&euro;&nbsp;${eventData.price}</p>
+      <ul class="trip-point__offers">
+        ${offersTemplate.join(``)}
+      </ul>
+    </article>
+  `;
+}
